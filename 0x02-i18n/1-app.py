28c70785729b1c47cf2331b65db00e086948ddf4
BrianTukei <briantukei1000@gmail.com>
2024-08-27 11:26:00 +0000

    0x02. i18n

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,9 +4,24 @@ A Basic flask application
 """
 from flask import Flask
 from flask import render_template
+from flask_babel import Babel
 
 
+class Config(object):
+    """
+    Application configuration class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# Instantiate the application object
 app = Flask(__name__)
+app.config.from_object(Config)
+
+# Wrap the application with Babel
+babel = Babel(app)
 
 
 @app.route('/', strict_slashes=False)
@@ -14,7 +29,7 @@ def index():
     """
     Renders a basic html template
     """
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == '__main__':
